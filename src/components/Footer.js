f39b478542b40a5434ddbbf53e3ae9ab92707656
bhargavikadagala <112746529+bhargavikadagala@users.noreply.github.com>
2023-11-02 11:56:08 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,14 @@
-function Footer(prop){
-	return(
-<div className="footer">
-          <p>Copyright 2022 - Vkive Tutorials</p>
-      </div>
-    )
-  }
-	export default Footer
+// external dependencies
+import React from 'react';
+
+/** This is a functional component for creating the footer of the application
+ */
+function BottomFooter(props) {
+
+	return (
+		<div className="footer">
+			<p>{ props.footerText }</p>
+		</div>
+	);
+}
+export default BottomFooter;
